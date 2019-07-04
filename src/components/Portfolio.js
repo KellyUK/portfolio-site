d30f79483a2ee19b201abdfd143211c03e5da9d2
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-07-04 09:22:22 +0100

    add project and links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Portfolio = () => {
-  return <div />;
+  return (
+    <div>
+      <section>
+        <h2>Northcoders News</h2>
+        <p>
+          A reddit style news aggregation site where readers can view articles
+          by topic. Each article can be rated up or down and users can leave
+          comments. Comments can also be deleted by their author. The front end
+          is built in React with Reach Router and using CSS Modules Stylesheets.
+          It accesses a server that I build in Node.js with Express and Knex
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default Portfolio;
